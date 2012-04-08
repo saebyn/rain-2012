d48edac08176a07633d5d705fc0ea280a82db52f
John Weaver <john@fornax.(none)>
2012-04-08 08:57:18 -0700

    Add README and copyright/license notices.

---
+++ README.rst
@@ -0,0 +1,13 @@
+
+RAIN
+====
+
+This is the source code for a 2d CofeeScript game I'm developing using gamejs.
+
+
+License
+-------
+
+The source files included in this repository are licensed under the MIT license.
+
+[http://www.opensource.org/licenses/mit-license.php]
--- assets/build.sh
+++ assets/build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+# ensure that the output directory for assets exists
 mkdir -p ../src/assets
+
+# copy in external assets
+if [ -d ../../rain-assets/assets ]; then
+    cp -ru ../../rain-assets/assets/* .
+fi
+
+# copy sprites into output directory
 cp -r sprites ../src/assets/
+
+# compile levels
 ./levels/build_all.py
